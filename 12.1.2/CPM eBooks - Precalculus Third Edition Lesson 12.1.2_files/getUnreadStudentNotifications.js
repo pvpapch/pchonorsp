42b5aedcb7c666de32bd3362bbd8e11499a012fe
pvpapch <162363320+pvpapch@users.noreply.github.com>
2024-03-05 18:29:53 -0500

    Add files via upload

---
+++ 12.1.2/CPM eBooks - Precalculus Third Edition Lesson 12.1.2_files/getUnreadStudentNotifications.js
@@ -0,0 +1,34 @@
+const element = document.getElementById("notification-badge");
+const HERMES_API_URL =
+  window.location.hostname === "ebooks.cpm.org"
+    ? "https://hermes.cpm.org"
+    : "https://hermes-api-test.cpm.org";
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+const getUnreadNotifications = async () => {
+  const cookie = getCookie("cpm-sso-token");
+
+  try {
+    const notifications = await fetch(HERMES_API_URL + "/api/student/studentAlert", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + cookie,
+      },
+    });
+
+    const data = await notifications.json();
+    const unarchivedAlerts = data.filter((alert) => alert.isArchived === false);
+    if (unarchivedAlerts.length) element.textContent = unarchivedAlerts.length;
+    
+    return;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+getUnreadNotifications();
